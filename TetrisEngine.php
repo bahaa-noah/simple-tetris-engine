f68a5e45869fd1db878d6127774e33d06fe517aa
Bahaa Noah <bahaa.noah93@gmail.com>
2022-07-20 00:50:10 +0400

    fix : filling the grid logic

--- TetrisEngine.php
+++ TetrisEngine.php
@@ -8,8 +8,7 @@ class TetrisEngine
     public array $data;
 
     private $maxRows = 10;
-    private $maxHeight = 5;
-    private $height = 0;
+    private $maxHeight = 100;
 
     private $shapes = [
         'Q' => [
@@ -37,13 +36,18 @@ class TetrisEngine
 
     private $grid = [];
 
-    public function initializePlayGround()
+
+    public function addRow()
     {
-        for ($i = 0; $i < $this->maxHeight; $i++) {
-            for ($j = 0; $j < $this->maxRows; $j++) {
-                $this->grid[$i][$j] = 0;
-            }
+        if (sizeOf($this->grid) > $this->maxHeight) {
+            throw new Exception("You have reached the maximum height, Game Over!");
         }
+
+        $row = [];
+        for ($j = 0; $j < $this->maxRows; $j++) {
+            $row[$j] = 0;
+        }
+        array_push($this->grid, $row);
     }
 
     public function formateFileInput()
@@ -65,74 +69,159 @@ class TetrisEngine
 
         $this->gridHeight = 0;
         $this->gridWidth = 0;
-
+        $result  = "";
         foreach ($data as $key => $value) {
             foreach ($value as $$key => $shape) {
                 $shapeArray = str_split($shape);
                 $shapeName  = $shapeArray[0];
                 $shapeStartIndex  = $shapeArray[1];
                 $this->fillTheGrid($this->shapes[$shapeName], $shapeStartIndex);
+                foreach ($this->grid as $key => $row) {
+                    if ($this->isCompletedRow($row)) {
+                        $this->removeRowByIndex($key);
+                    }
+                }
             }
 
-            $result = $this->height;
-            print ($result) . PHP_EOL;
+            $result .= $this->getGridHeight() . ",";
             $this->resetGrid();
         }
+
+        print ($result) . PHP_EOL;
+        $this->saveOutput($result);
+    }
+
+
+    public function saveOutput($result)
+    {
+        $output = fopen("output.txt", "w") or die("Unable to open file!");
+        fwrite($output, $result . "\n");
+        fclose($output);
     }
 
 
     public function resetGrid()
     {
-        $this->height = 0;
         unset($this->grid);
-        $this->initializePlayGround();
+        $this->grid = [];
     }
 
 
     public function fillTheGrid(array $shape, int $startIndex)
     {
 
-        $i = 0;
-        $gridRow = 0;
-        while ($i < sizeof($shape)) {
 
-            $shapeRow = $shape[$i][0];
-            $shapeCol = $shape[$i][1] + $startIndex;
-            $gridRow = $shapeRow;
-            $gridCol = $shapeCol;
-            $isAvailable = $this->grid[$gridRow][$gridCol] == 0;
+        if (empty($this->grid)) {
+            $this->addRow();
+        }
 
-            if ($isAvailable) {
-                $this->grid[$gridRow][$gridCol] = 1;
-            } else {
+        $formattedShape = [];
+        for ($i = 0; $i < sizeof($shape); $i++) {
+            $formattedShape[$i][0] =  $shape[$i][0];
+            $formattedShape[$i][1] =  $shape[$i][1] + $startIndex;
+        }
 
-                while ($this->grid[$gridRow][$gridCol] == 1) {
-                    $gridRow++;
-                    if ($this->grid[$gridRow][$gridCol] == 0) {
-                        $this->grid[$gridRow][$gridCol] = 1;
-                        break;
-                    }
+        foreach ($this->grid as $key => $row) {
+            if ($row[$startIndex] == 1) {
+                for ($i = 0; $i < sizeof($shape); $i++) {
+                    $formattedShape[$i][0] =  $shape[$i][0] + $key;
+                    $formattedShape[$i][1] =  $shape[$i][1] + $startIndex;
                 }
             }
-            $this->height = $gridRow + 1;
-            $i++;
+        }
 
-            if ($this->isCompletedRow($this->grid[0])) {
-                $this->height--;
-                $this->removeRowByIndex(0);
+        $gridRow = $formattedShape[0][0];
+        $gridCol = $formattedShape[0][1];
+
+        while (!isset($this->grid[$gridRow][$gridCol])) {
+            $this->addRow();
+        }
+
+        while ($this->grid[$gridRow][$gridCol] == 1) {
+
+            $gridRow++;
+            if (!isset($this->grid[$gridRow][$gridCol])) {
+                $this->addRow();
+            }
+
+            for ($j = 0; $j < sizeof($formattedShape); $j++) {
+                $formattedShape[$j][0]++;
             }
         }
-        print_r($this->grid);
+
+
+        for ($i = 0; $i < sizeof($formattedShape); $i++) {
+            if (!isset($this->grid[$formattedShape[$i][0]][$formattedShape[$i][1]])) {
+                $this->addRow();
+            }
+        }
+
+        for ($k = 0; $k < count($formattedShape); $k++) {
+            $this->grid[$formattedShape[$k][0]][$formattedShape[$k][1]] = 1;
+        }
     }
+
+    // public function fillTheGrid(array $shape, int $startIndex)
+    // {
+
+
+    //     if (empty($this->grid)) {
+    //         $this->addRow();
+    //     }
+
+    //     $i = 0;
+    //     $gridRow = 0;
+    //     while ($i < sizeof($shape)) {
+
+    //         $shapeRow = $shape[$i][0];
+    //         $shapeCol = $shape[$i][1] + $startIndex;
+    //         $gridRow = $shapeRow;
+    //         $gridCol = $shapeCol;
+
+    //         if ($gridCol >= $this->maxRows) {
+    //             throw new Exception("Illegal shape start Index, Please enter valid start and try again!");
+    //             die;
+    //         }
+
+    //         while (!isset($this->grid[$gridRow][$gridCol])) {
+    //             $this->addRow();
+    //         }
+
+    //         $isAvailable = $this->grid[$gridRow][$gridCol] == 0;
+
+    //         if ($isAvailable) {
+    //             $this->grid[$gridRow][$gridCol] = 1;
+    //         } else {
+
+    //             if (!isset($this->grid[$gridRow][$gridCol])) {
+    //                 $this->addRow();
+    //             }
+
+    //             while ($this->grid[$gridRow][$gridCol] == 1) {
+
+    //                 $gridRow++;
+
+    //                 if (!isset($this->grid[$gridRow][$gridCol])) {
+    //                     $this->addRow();
+    //                 }
+
+    //                 if ($this->grid[$gridRow][$gridCol] == 0) {
+    //                     $this->grid[$gridRow][$gridCol] = 1;
+    //                     break;
+    //                 }
+    //             }
+    //         }
+    //         $i++;
+    //     }
+
+    //     if ($this->isCompletedRow($this->grid[0])) {
+    //         $this->removeRowByIndex(0);
+    //     }
+    // }
 
     public function isCompletedRow($row)
     {
-        foreach ($row as $value) {
-            if ($value !== 1) {
-                return false;
-            }
-        }
-        return true;
+        return in_array(0, $row) ? false : true;
     }
 
     public function removeRowByIndex($index)
@@ -154,7 +243,7 @@ class TetrisEngine
 
 
 $tetrisEngine =  new TetrisEngine;
-$tetrisEngine->initializePlayGround();
 $tetrisEngine->formateFileInput();
 $tetrisEngine->createGrid();
-$tetrisEngine->getGridHeight();
+$result = $tetrisEngine->getGrid();
+// print_r($result);
